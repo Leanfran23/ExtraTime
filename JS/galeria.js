a322fee6117646d0279479a29c73ea150e915708
Leafranco23 <leafranco90@gmail.com>
2022-07-27 23:28:48 -0300

    Se agregaron partes del responsive

--- JS/galeria.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', ()=> {
-  const imgLightbox = document.querySelectorAll('.materialboxed');
-  M.Materialbox.init(imgLightbox, {
-      intDuration: 900,
-      outDuration: 1500
-  });
-});
