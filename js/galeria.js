f18286b15cd234bcd0dcd750a47bcec70b2df964
Leafranco23 <leafranco90@gmail.com>
2022-08-04 12:42:20 -0300

    se agrega js para la galeria

---
+++ js/galeria.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', ()=> {
+    const imgLightbox = document.querySelectorAll('.materialboxed');
+    M.Materialbox.init(imgLightbox, {
+        intDuration: 900,
+        outDuration: 1500
+    });
+});
